bf712ae1626cd8505399440ca4580472fde372a7
marian <marian.muller@serli.com>
2024-02-23 00:26:59 +0100

    clean command handling

--- joybus.h
+++ joybus.h
@@ -20,8 +20,6 @@ void joybus_port_init_rx(joybus_port_t *port_rx, uint pin, irq_handler_t handler
 void joybus_port_init_tx(joybus_port_t *port_tx, uint pin);
 void joybus_port_terminate_rx(joybus_port_t *port_rx);
 void joybus_port_terminate_tx(joybus_port_t *port_tx);
-//void joybus_port_reset(joybus_port_t *port_rx, joybus_port_t *port_tx);
-//void joybus_register_rx_handler(joybus_port_t *port_rx, irq_handler_t handler);
 void joybus_send_bytes(joybus_port_t *port_tx, uint8_t *bytes, uint len);
 void joybus_send_byte(joybus_port_t *port_tx, uint8_t byte, bool stop);
 
